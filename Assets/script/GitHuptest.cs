211131c05b6e4ceaad1e8d935de938a0d91279f4
Abdallah Badarneh <abdallah.badarne7@gmail.com>
2024-10-28 16:38:46 +0200

    first commit

---
+++ Assets/script/GitHuptest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGenerator
+{
+    public static void Main()
+    {
+        Random random = new Random();
+        int number;
+
+        while (true)
+        {
+            number = random.Next(1, 21); 
+            if (number == 5)
+            {
+                continue; 
+            }
+            Console.WriteLine(number);
+            if (number == 15)
+            {
+                break; 
+            }
+        }
+    }
+}
